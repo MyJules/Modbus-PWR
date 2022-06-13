2263a359a2d1d9bb1143620e928e3df2fb008903
MyJules <48486998+MyJules@users.noreply.github.com>
2022-06-14 00:51:23 +0200

    Two buffer echo

--- Nucleo_Config/Nucleo_config/Core/Src/mdbus_slave/mdbus_slave.c
+++ Nucleo_Config/Nucleo_config/Core/Src/mdbus_slave/mdbus_slave.c
@@ -11,16 +11,17 @@ void reception();
 void wait_end_of_frame();
 void bad_mdbus_state();
 
-ring_buffer_t message_queue;
-mdbus_State mdbus_state = IDLE;
+ring_buffer_t m_message_queue_send;
+ring_buffer_t m_message_queue_receive;
+mdbus_State m_mdbus_state = IDLE;
 
 void mdbus_on_packet_send()
 {
-	if(ring_buffer_is_empty(&message_queue)) return;
+	if(ring_buffer_is_empty(&m_message_queue_send)) return;
 
 	uint8_t data[PACKETSIZE];
 	//read data from buffer
-	ring_buffer_dequeue_arr(&message_queue, data, PACKETSIZE);
+	ring_buffer_dequeue_arr(&m_message_queue_send, data, PACKETSIZE);
 	//send queued package
 	mdbus_send_packet_it(data, PACKETSIZE);
 
@@ -28,13 +29,13 @@ void mdbus_on_packet_send()
 
 void mdbus_on_packet_receive(uint8_t *data, uint16_t size)
 {
-	ring_buffer_queue_arr(&message_queue, data, PACKETSIZE);
+	ring_buffer_queue_arr(&m_message_queue_receive, data, PACKETSIZE);
 	mdbus_read_packet_it(data, size);
 }
 
 void mdbus_slave_run()
 {
-	switch(mdbus_state)
+	switch(m_mdbus_state)
 	{
 	case IDLE:
 		idle();
@@ -46,11 +47,17 @@ void mdbus_slave_run()
 
 void mdbus_slave_configure()
 {
-	mdbus_state = IDLE;
-	ring_buffer_init(&message_queue);
+	m_mdbus_state = IDLE;
+	ring_buffer_init(&m_message_queue_send);
+	ring_buffer_init(&m_message_queue_receive);
 }
 
 void idle()
 {
+	if(ring_buffer_is_empty(&m_message_queue_receive)) return;
+
+	uint8_t data[PACKETSIZE];
+	ring_buffer_dequeue_arr(&m_message_queue_receive, data, PACKETSIZE);
+	ring_buffer_queue_arr(&m_message_queue_send, data, PACKETSIZE);
 }
 
